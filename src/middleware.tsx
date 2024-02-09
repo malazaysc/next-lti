192006b93198ce25d4ccd12590eeba86c9bccbd4
malazaysc <alexisgiovoglanian@southerncode.us>
2024-02-09 00:44:17 -0300

    Middleware

---
+++ src/middleware.tsx
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    console.log('middleware request.url', request.url)
+    console.log('middleware request.headers', request.headers)
+    const response = NextResponse.next();
+    response.headers.set('Content-Security-Policy', "frame-ancestors 'self' https://canvas.instructure.com");
+
+
+    return response;
+}
+ 
