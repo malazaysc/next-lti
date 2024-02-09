b77cd8153734c6322944b4d07f6a62b07188f032
malazaysc <alexisgiovoglanian@southerncode.us>
2024-02-09 15:34:33 -0300

    client side

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,15 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*)',
+                source: '/lti',
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                    { key: "X-Frame-Options", value: "ALLOW-FROM https://canvas.instructure.com"}
+                    { key: "X-Frame-Options", value: "ALLOW-FROM https://*.instructure.com"},
+                    { key: "Content-Security-Policy", value: "frame-ancestors 'self' https://*.instructure.com"},
+                    { key: "X-Content-Type-Options", value: "nosniff"},
                 ]
             }
         ]
